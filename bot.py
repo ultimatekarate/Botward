a386adf81a7f336502c5818c8ebdf62c8dde9213
Joe Volzer <joevolzer@gmail.com>
2022-07-14 15:41:30 -0400

    error handling

--- bot.py
+++ bot.py
@@ -30,32 +30,34 @@ async def Deadward(ctx):
   await ctx.channel.send(embed=shutdown_embed)
 
 @bot.command(pass_context=True)
-async def a(ctx, channel_name='raid-chat', filename=None):
+async def attendance(ctx, channel_name=None, raid_mob=None):
+
+    if channel_name is None or raid_mob is None:
+      message = "Use the following command to take attendance: \n **!attendance \"<voice channel>\" \"<raid mob>\"**"
+      name_embed = discord.Embed(title='MISSING ARGUMENTS',description=message,color=0xFF0000)
+      await ctx.channel.send(embed=name_embed)
+      return
+
     for channel in ctx.guild.voice_channels:
         if channel.name == channel_name:
-            message = 'Members in '+channel_name
-            await ctx.send(message)
-
-            if filename is None:
-              f_name = 'raid-attendance'
-            else:
-              f_name = filename
-            
-            attendance_file = open(f_name+'.csv','w+')
+            f_name = raid_mob+'.csv'
+            attendance_file = open(f_name,'w+')
 
             for member in channel.members:
               member_name = member.name + '#' + member.discriminator
               char = CHARACTER_MAPPING.get(member_name)
               if not char:
                 message = "Discord name " + member_name + " was not found in the mapping."
-                name_embed = discord.Embed(title='NAME NOT FOUND',description =message,color=0xFF0000)
+                name_embed = discord.Embed(title='NAME NOT FOUND',description = message,color=0xFF0000)
                 await ctx.channel.send(embed=name_embed)
               else:
                 attendance_file.write(f"1\t{char}\t65\tBard\tYes\n")
+            
             attendance_file.close()
-
-            file = discord.File(f_name+'.csv')
-            await ctx.send(file=file, content="Message to be sent")
+            raid_time = datetime.datetime.now().strftime('%D %H:%M')
+            file = discord.File(f_name)
+            attachment_message = "Attendance taken in "+channel_name+" for "+ raid_mob + " at "+str(raid_time)+". \n" + str(len(channel.members)) + " nerds accounted for."
+            await ctx.send(file=file, content=attachment_message)
               
 
 bot.run(TOKEN)
