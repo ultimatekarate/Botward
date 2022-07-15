334704b37ad5e5ad1857bbea31142cd461cb8b50
Joe Volzer <joevolzer@gmail.com>
2022-07-15 18:22:24 -0400

    you may now ask botward what it knows

--- bot.py
+++ bot.py
@@ -52,6 +52,12 @@ async def on_message(message):
   await bot.process_commands(message)
 
 @bot.command(pass_context=True)
+async def questions(ctx):
+  message = QUESTIONS.keys.join('\n')
+  embed = discord.Embed(title='BOTWARD\'S SECRET KNOWLEDGE',description=message,color=0xFF000)
+  await ctx.channel.send(embed=embed)
+  
+@bot.command(pass_context=True)
 async def reload_mapping(ctx):
   # This function reloads the character mapping file.
   global CHARACTER_MAPPING
